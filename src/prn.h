9e290b785267e0a393713b296f8aef7c9b6fd490
Alexei Colin <ac@alexeicolin.com>
2015-12-17 11:54:04 -0500

    factor out prn seqs into a file; add config vars

---
+++ src/prn.h
@@ -0,0 +1,8 @@
+#ifndef LIBSPRITE_PRN_H
+#define LIBSPRITE_PRN_H
+
+/* A pair of PRN arrays for communication using Gold codes */
+static unsigned char PRN_0[];
+static unsigned char PRN_1[];
+
+#endif // LIBSPRITE_PRN_H
